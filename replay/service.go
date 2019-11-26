653874f8c2bd3b5ff090e6069d576f0fe0fe8a63
adrianwit@gmail.com <Ubc!1234F>
2019-11-25 20:28:47 -0800

    Revert "updated event id from window"
    This reverts commit 974c22cd13b87a812f6cb00d108883738b80dc74.

--- replay/service.go
+++ replay/service.go
@@ -47,10 +47,6 @@ func (s *service) replay(ctx context.Context, request *Request, response *Respon
 	}
 	objects, err := s.list(ctx, request.TriggerURL, request.unprocessedModifiedBefore)
 	fmt.Printf("%v %v\n", request.TriggerURL, len(objects))
-
-	mover := newMover(s.fs)
-	mover.Run(ctx, 5)
-
 	for i := range objects {
 		if objects[i].IsDir() {
 			continue
@@ -64,13 +60,25 @@ func (s *service) replay(ctx context.Context, request *Request, response *Respon
 			continue
 		}
 
-		mover.Schedule(&replay{
-			src:  sourceURL,
-			dest: destURL,
-		})
+		go func(sourceURL, destURL string) {
+
+			if err := s.fs.Move(ctx, sourceURL, destURL); err != nil {
+				fmt.Printf("failed to move %v\n", err)
+				return
+			}
+			if err := s.fs.Move(ctx, destURL, sourceURL); err != nil {
+				fmt.Printf("failed to move %v\n", err)
+				return
+			}
+			fmt.Printf("replayed %v\n", sourceURL)
+			response.Replayed = append(response.Replayed, sourceURL)
+			if err := s.fs.Upload(ctx, replayedURL, 0644, strings.NewReader(sourceURL)); err != nil {
+				return
+			}
+		}(sourceURL, destURL)
 
 	}
-	return mover.Wait()
+	return nil
 }
 
 func (s *service) list(ctx context.Context, URL string, modifiedBefore *time.Time) ([]storage.Object, error) {
