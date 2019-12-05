2dcfc7fcb8d6593f74780f0bbbd7122ecf813a12
adrianwit@gmail.com <Ubc!1234F>
2019-12-05 14:46:13 -0800

    removed compilation issue

--- monitor.go
+++ monitor.go
@@ -1,13 +1,6 @@
 package bqtail
 
 import (
-	"bqtail/base"
-	"bqtail/mon"
-	"context"
-	"encoding/json"
-	"fmt"
-	"github.com/pkg/errors"
-	"log"
 	"net/http"
 )
 
@@ -18,33 +11,6 @@ func BqTailMonitor(w http.ResponseWriter, r *http.Request) {
 			_ = r.Body.Close()
 		}()
 	}
-	err := checkStorage(w, r)
-	if err != nil {
-		log.Print(err)
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	//work in progress...
 }
 
-func checkStorage(writer http.ResponseWriter, httpRequest *http.Request) (err error) {
-	defer func() {
-		if r := recover(); r != nil {
-			err = fmt.Errorf("%v", r)
-		}
-	}()
-	request := &mon.Request{}
-	if err = json.NewDecoder(httpRequest.Body).Decode(&request); err != nil {
-		return errors.Wrapf(err, "failed to decode %T", request)
-	}
-	ctx := context.Background()
-	service, err := mon.NewFromEnv(ctx, base.ConfigEnvKey)
-	if err != nil {
-		return err
-	}
-	response := service.Check(ctx, request)
-	if err = json.NewEncoder(writer).Encode(response); err != nil {
-		return err
-	}
-	return err
-}
