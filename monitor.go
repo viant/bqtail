6a3a58e1be0ec8b7eaf77de8a7d9427e9f4af28e
adrianwit@gmail.com <Ubc!1234F>
2019-12-09 13:41:03 -0800

    updated monitor entry point

--- monitor.go
+++ monitor.go
@@ -1,6 +1,13 @@
 package bqtail
 
 import (
+	"bqtail/base"
+	"bqtail/mon"
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/pkg/errors"
+	"log"
 	"net/http"
 )
 
@@ -11,5 +18,33 @@ func BqTailMonitor(w http.ResponseWriter, r *http.Request) {
 			_ = r.Body.Close()
 		}()
 	}
-	//work in progress...
+	err := checkStorage(w, r)
+	if err != nil {
+		log.Print(err)
+	}
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func checkStorage(writer http.ResponseWriter, httpRequest *http.Request) (err error) {
+	defer func() {
+		if r := recover(); r != nil {
+			err = fmt.Errorf("%v", r)
+		}
+	}()
+	request := &mon.Request{}
+	if err = json.NewDecoder(httpRequest.Body).Decode(&request); err != nil {
+		return errors.Wrapf(err, "failed to decode %T", request)
+	}
+	ctx := context.Background()
+	service, err := mon.Singleton(ctx, base.ConfigEnvKey)
+	if err != nil {
+		return err
+	}
+	response := service.Check(ctx, request)
+	if err = json.NewEncoder(writer).Encode(response); err != nil {
+		return err
+	}
+	return err
 }
