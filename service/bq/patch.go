c87d12334293326f50fbd25c00a4b8a9accf811c
adrianwit <adrianwit@gmail.com>
2020-02-27 14:40:00 -0800

    added patch support

---
+++ service/bq/patch.go
@@ -0,0 +1,85 @@
+package bq
+
+import (
+	"context"
+	"fmt"
+	"github.com/pkg/errors"
+	"github.com/viant/bqtail/base"
+	"github.com/viant/bqtail/shared"
+	"google.golang.org/api/bigquery/v2"
+	"time"
+)
+
+//Patch patch temp table
+func (s *service) Patch(ctx context.Context, request *PatchRequest) (*bigquery.Table, error) {
+	if err := request.Init(s.projectID); err != nil {
+		return nil, err
+	}
+	if err := request.Validate(); err != nil {
+		return nil, err
+	}
+	tableRef, err := base.NewTableReference(request.Table)
+	if err != nil {
+		return nil, errors.Wrapf(err, "invalid table: %v", request.Table)
+	}
+	if tableRef.ProjectId == "" {
+		tableRef.ProjectId = s.ProjectID
+	}
+
+	if request.TemplateTable == nil {
+		templateRef, err := base.NewTableReference(request.Template)
+		if err != nil {
+			return nil, errors.Wrapf(err, "invalid template table: %v", request.Template)
+		}
+		request.TemplateTable, err = s.Table(ctx, templateRef)
+		if err != nil {
+			return nil, errors.Wrapf(err, "invalid get template table: %v", request.Table)
+		}
+	}
+
+	call := s.Service.Tables.Patch(tableRef.ProjectId, tableRef.DatasetId, tableRef.TableId, request.TemplateTable)
+	call.Context(ctx)
+	var table *bigquery.Table
+	for i := 0; i < shared.MaxRetries; i++ {
+		call.Context(ctx)
+		if table, err = call.Do(); err == nil {
+			return nil, err
+		}
+		if base.IsRetryError(err) {
+			//do extra sleep before retrying
+			time.Sleep(shared.RetrySleepInSec * time.Second)
+			continue
+		}
+		break
+	}
+	return table, err
+}
+
+//PatchRequest represents an export request
+type PatchRequest struct {
+	Template      string
+	Table         string
+	TemplateTable *bigquery.Table
+	ProjectID     string
+}
+
+//Init initialises request
+func (r *PatchRequest) Init(projectID string) (err error) {
+	if r.ProjectID != "" {
+		projectID = r.ProjectID
+	} else {
+		r.ProjectID = projectID
+	}
+	return nil
+}
+
+//Validate checks if request is valid
+func (r *PatchRequest) Validate() error {
+	if r.Template == "" && r.TemplateTable == nil {
+		return fmt.Errorf("template was empty")
+	}
+	if r.Table == "" {
+		return fmt.Errorf("destination was empty")
+	}
+	return nil
+}
