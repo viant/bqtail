dc3ae5090168cca2bc2c2d46a20d2707a4daaa50
Adrian Witas <adrianwit@gmail.com>
2020-04-05 16:42:37 -0700

    udpated test

--- e2e/regression/cases/039_cli_stress_test/bqtail/schema.sql
+++ e2e/regression/cases/039_cli_stress_test/bqtail/schema.sql
@@ -2,7 +2,7 @@
 CREATE OR REPLACE TABLE dummy_v${parentIndex}_tmpl (
       id         INT64 NOT NULL,
       type_id    INT64 NOT NULL,
-      event_id   INT64 NOT NULL,
+      event_type   INT64 NOT NULL,
       name       STRING
 );
 
