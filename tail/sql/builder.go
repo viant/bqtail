d7d18db33bf70d2a7bedc91ac290f79dde321105
Adrian Witas <adrianwit@gmail.com>
2020-04-08 16:03:21 -0700

    updated transform expresison

--- tail/sql/builder.go
+++ tail/sql/builder.go
@@ -5,6 +5,7 @@ import (
 	"github.com/viant/bqtail/base"
 	"github.com/viant/bqtail/tail/config"
 	"google.golang.org/api/bigquery/v2"
+	"sort"
 	"strings"
 )
 
@@ -12,15 +13,25 @@ func buildNestedDedupeSQL(sourceTable string, schema Schema, dest *config.Destin
 	projection := make([]string, 0)
 	innerProjection := make([]string, 0)
 
+	transform, transformKeys := getTransform(dest)
 	for _, field := range schema.Fields {
 		projection = append(projection, field.Name)
-
 		if expression, ok := getTransformExpression(dest, field); ok {
+			delete(transform, strings.ToLower(field.Name))
 			innerProjection = append(innerProjection, fmt.Sprintf("%v AS %v", expression, field.Name))
 			continue
 		}
 		innerProjection = append(innerProjection, fmt.Sprintf("%v.%v AS %v", dest.Transient.Alias, field.Name, field.Name))
 	}
+	for _, key := range transformKeys {
+		expression, ok := transform[strings.ToLower(key)]
+		if ! ok {
+			continue
+		}
+		projection = append(projection, key)
+		innerProjection = append(innerProjection, fmt.Sprintf("%v AS %v", expression, key))
+	}
+
 	return fmt.Sprintf(`SELECT %v
 FROM (
   SELECT
@@ -29,6 +40,19 @@ FROM (
   FROM %v $WHERE
 ) %v $JOIN
 WHERE row_number = 1`, strings.Join(innerProjection, ", "), strings.Join(projection, ", "), strings.Join(dest.UniqueColumns, ","), sourceTable, dest.Transient.Alias)
+}
+
+func getTransform(dest *config.Destination) (map[string]string, []string) {
+	transform := make(map[string]string)
+	transformKeys := make([]string, 0)
+	if len(dest.Transform) > 0 {
+		for k, v := range dest.Transform {
+			transform[strings.ToLower(k)] = v
+			transformKeys = append(transformKeys, k)
+		}
+	}
+	sort.Strings(transformKeys)
+	return transform, transformKeys
 }
 
 func columnNames(schema *bigquery.TableSchema) []string {
@@ -42,6 +66,10 @@ func columnNames(schema *bigquery.TableSchema) []string {
 func buildDedupeSQL(sourceTable string, schema Schema, unique map[string]bool, dest *config.Destination) string {
 	var projection = make([]string, 0)
 	var groupBy = make([]string, 0)
+
+
+	transform, transformKeys := getTransform(dest)
+
 	for i, field := range schema.Fields {
 		if unique[strings.ToLower(field.Name)] {
 			groupBy = append(groupBy, fmt.Sprintf("%d", i+1))
@@ -50,10 +78,20 @@ func buildDedupeSQL(sourceTable string, schema Schema, unique map[string]bool, d
 		}
 		expression := fmt.Sprintf("%v.%v", dest.Transient.Alias, field.Name)
 		if transformExpression, ok := getTransformExpression(dest, field); ok {
+			delete(transform, strings.ToLower(field.Name))
 			expression = transformExpression
 		}
 		projection = append(projection, fmt.Sprintf("MAX(%v) AS %v", expression, field.Name))
 	}
+
+	for _, key := range transformKeys {
+		expression, ok := transform[strings.ToLower(key)]
+		if ! ok {
+			continue
+		}
+		projection = append(projection, fmt.Sprintf("MAX(%v) AS %v", expression, key))
+	}
+
 	return fmt.Sprintf(`SELECT %v 
 FROM %v %v $JOIN
 $WHERE
@@ -67,15 +105,23 @@ GROUP BY %v`,
 
 func buildSelectAll(sourceTable string, schema Schema, dest *config.Destination) string {
 	var projection = make([]string, 0)
+	transform, transformKeys := getTransform(dest)
 	for _, field := range schema.Fields {
-
 		if expression, ok := getTransformExpression(dest, field); ok {
+			delete(transform, strings.ToLower(field.Name))
 			projection = append(projection, fmt.Sprintf("%v AS %v", expression, field.Name))
 			continue
 		}
-
 		projection = append(projection, fmt.Sprintf("%v.%v AS %v", dest.Transient.Alias, field.Name, field.Name))
 	}
+	for _, key := range transformKeys {
+		expression, ok := transform[strings.ToLower(key)]
+		if ! ok {
+			continue
+		}
+		projection = append(projection, fmt.Sprintf("%v AS %v", expression, key))
+	}
+
 	return fmt.Sprintf(`SELECT %v 
 FROM %v %v $JOIN $WHERE`,
 		strings.Join(projection, ", "),
