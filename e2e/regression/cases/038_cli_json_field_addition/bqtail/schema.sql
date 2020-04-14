0ec723049dc1e005c4971fa01a55921bf3f9e36f
Adrian Witas <adrianwit@gmail.com>
2020-04-13 22:16:24 -0700

    updated test with nested schema

--- e2e/regression/cases/038_cli_json_field_addition/bqtail/schema.sql
+++ e2e/regression/cases/038_cli_json_field_addition/bqtail/schema.sql
@@ -1,5 +1,18 @@
+CREATE OR REPLACE TABLE dummy_v${parentIndex} (
+      id         INT64 NOT NULL,
+      type_id    INT64 NOT NULL,
+      params ARRAY<STRUCT<
+        key string,
+        text_value STRING>>
+ );
+
+
+
 CREATE OR REPLACE TABLE dummy_v${parentIndex}_tmpl (
       id         INT64 NOT NULL,
-      type_id    INT64 NOT NULL
+      type_id    INT64 NOT NULL,
+      params ARRAY<STRUCT<
+        key string,
+        text_value STRING >>
  );
 
