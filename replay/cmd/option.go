50c8a7114d5422877c066934f06793ccab8074ad
Adrian Witas <adrianwit@gmail.com>
2020-04-18 15:26:15 -0700

    updated service

--- replay/cmd/option.go
+++ replay/cmd/option.go
@@ -1,1 +1,24 @@
 package cmd
+
+
+type Options struct {
+	Version bool `short:"v" long:"version" description:"bqtail version"`
+
+	ProjectID string `short:"p" long:"project" description:"Google Cloud Project"`
+
+	Bucket string `short:"b" long:"bucket" description:"Google Cloud Storage ingestion bucket"`
+
+	Method string `short:"m" long:"bucket" description:"Google Cloud Storage ingestion bucket"`
+
+	SourceURL string `short:"s" long:"src" description:"source URL" `
+
+	TriggerBucket string `short:"s" long:"src" description:"source URL" `
+
+	OperationBucket string `short:"s" long:"src" description:"source URL" `
+
+	DryRun bool `short:"d" long:"dry" description:"dry run flag" `
+
+	Client string `short:"c" long:"client" description:"GCP OAuth client url"`
+
+}
+
