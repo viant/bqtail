f3c17506491603726370d4ba63600e1d398d738e
Adrian Witas <adrianwit@gmail.com>
2020-04-18 15:49:51 -0700

    updated options

--- replay/cmd/option.go
+++ replay/cmd/option.go
@@ -2,23 +2,21 @@ package cmd
 
 
 type Options struct {
+
 	Version bool `short:"v" long:"version" description:"bqtail version"`
 
 	ProjectID string `short:"p" long:"project" description:"Google Cloud Project"`
 
-	Bucket string `short:"b" long:"bucket" description:"Google Cloud Storage ingestion bucket"`
+	Method string `short:"m" long:"method" choice:"data" choice:"proc" description:"replay based on failed process or stalled datafiles"`
 
-	Method string `short:"m" long:"bucket" description:"Google Cloud Storage ingestion bucket"`
+	Age  string `short:"a" long:"age" description:"min unprocess datafile age expression like: 60 min"`
 
 	SourceURL string `short:"s" long:"src" description:"source URL" `
 
-	TriggerBucket string `short:"s" long:"src" description:"source URL" `
-
-	OperationBucket string `short:"s" long:"src" description:"source URL" `
+	TriggerBucket string `short:"t" long:"src" description:"source URL" `
 
 	DryRun bool `short:"d" long:"dry" description:"dry run flag" `
 
 	Client string `short:"c" long:"client" description:"GCP OAuth client url"`
 
 }
-
